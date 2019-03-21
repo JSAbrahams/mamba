d0ecf36d186b1d2b1dcb8d5a98947234dfbdcf83
Joël Abrahams <JSAbrahams@users.noreply.github.com>
2019-03-21 15:56:25 +0100

    WIP Update CodeCov configuration (#43)
    * Add bors and point it to travis build
    
    * Only build master in Appveyor and cache in Travis
    
    This should speed up builds. We don't build for every push now.
    We also cache cargo in Travis. Not sure if this will work properly but
    we'll see.
    
    * Update codecov config
    
    - Require ci to pass
    - Use sunburst diagram and list files in comment layout
    - Change range to 50..100
    - Notify if coverage changes in file not touched by PR
    
    * Add example tests for foreach statements
    
    This was also added here to test the new codecov config.
    
    * Format control flow tests

--- tests/parser/control_flow.rs
+++ tests/parser/control_flow.rs
@@ -1,6 +1,47 @@
 use crate::util::*;
 use mamba::lexer::tokenize;
+use mamba::parser::ast::ASTNode;
 use mamba::parser::parse;
+use mamba::parser::parse_direct;
+
+#[test]
+fn parse_for_statement_check() {
+    let source = String::from("foreach a in c do d");
+    let ast_tree = parse_direct(&tokenize(&source).unwrap()).unwrap();
+
+    match ast_tree.node {
+        ASTNode::Script { statements, .. } =>
+            match &statements.first().expect("script empty.").node {
+                ASTNode::For { expr, collection, body } => {
+                    assert_eq!(expr[0].node, ASTNode::Id { lit: String::from("a") });
+                    assert_eq!(collection.node, ASTNode::Id { lit: String::from("c") });
+                    assert_eq!(body.node, ASTNode::Id { lit: String::from("d") });
+                }
+                _ => panic!("first element script was not for loop.")
+            },
+        _ => panic!("ast_tree was not script.")
+    };
+}
+
+#[test]
+fn parse_for_statement_multiple_expr_check() {
+    let source = String::from("foreach a,b in c do d");
+    let ast_tree = parse_direct(&tokenize(&source).unwrap()).unwrap();
+
+    match ast_tree.node {
+        ASTNode::Script { statements, .. } =>
+            match &statements.first().expect("script empty.").node {
+                ASTNode::For { expr, collection, body } => {
+                    assert_eq!(expr[0].node, ASTNode::Id { lit: String::from("a") });
+                    assert_eq!(expr[1].node, ASTNode::Id { lit: String::from("b") });
+                    assert_eq!(collection.node, ASTNode::Id { lit: String::from("c") });
+                    assert_eq!(body.node, ASTNode::Id { lit: String::from("d") });
+                }
+                _ => panic!("first element script was not for loop.")
+            },
+        _ => panic!("ast_tree was not script.")
+    };
+}
 
 #[test]
 fn parse_for_statements() {
