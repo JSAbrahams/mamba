87b7d7880e8ebbdb3df9c117b58c2a4290ac5c76
Joel Abrahams <abrahamsjo@gmail.com>
2019-04-26 19:43:48 +0200

    Remove unnecessary check that tuple Python file exists
    If an error occurred it should have been caught

--- tests/output/syntax.rs
+++ tests/output/syntax.rs
@@ -6,24 +6,19 @@ use std::process::Command;
 #[test]
 fn output_tuple_valid_syntax() {
     let source = valid_resource_path(&["collection"], "tuple.mamba");
-    let path = &mut Path::new(&source);
-
-    let path = mamba_to_python_direct(path).unwrap();
+    let path = mamba_to_python_direct(Path::new(&source)).unwrap();
     let mut cmd = Command::new("py").arg("-m").arg("py_compile").arg(path).output().unwrap();
 
     if cmd.status.code().unwrap() != 0 {
         panic!("{}", String::from_utf8(cmd.stderr).unwrap());
     }
-    check_valid_resource_exists_and_delete(&["collection"], "tuple.py");
 }
 
 #[test]
 #[ignore]
 fn output_class_valid_syntax() {
     let source = valid_resource_path(&["class"], "class.mamba");
-    let path = &mut Path::new(&source);
-
-    let path = mamba_to_python_direct(path).unwrap();
+    let path = mamba_to_python_direct(Path::new(&source)).unwrap();
     let mut cmd = Command::new("py").arg("-m").arg("py_compile").arg(path).output().unwrap();
 
     if cmd.status.code().unwrap() != 0 {
