e3377403fef5023f6afbed21855f6afc6df2203a
Joel Abrahams <abrahamsjo@gmail.com>
2019-05-20 10:47:40 +0200

    Add tests for incorrectly formatted handle

---
+++ tests/parser/invalid/error.rs
@@ -0,0 +1,14 @@
+use mamba::lexer::tokenize;
+use mamba::parser::parse_direct;
+
+#[test]
+fn handle_no_branches() {
+    let source = String::from("def a handle");
+    parse_direct(&tokenize(&source).unwrap()).unwrap_err();
+}
+
+#[test]
+fn handle_no_indentation() {
+    let source = String::from("def a handle\nerr: Err => b");
+    parse_direct(&tokenize(&source).unwrap()).unwrap_err();
+}
