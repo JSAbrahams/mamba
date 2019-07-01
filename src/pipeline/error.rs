65fbdd65849f3d1c9a48b5554d9f43905163e8b6
Joel Abrahams <abrahamsjo@gmail.com>
2019-07-01 16:50:24 +0200

    If nested errors on same line don't print source code again

--- src/pipeline/error.rs
+++ src/pipeline/error.rs
@@ -10,19 +10,31 @@ pub fn syntax_err(err: &ParseErr, source: &str, in_path: &PathBuf) -> String {
     let trimmed_causes = &err.causes[0..min(err.causes.len(), SYNTAX_ERR_MAX_DEPTH)];
 
     let mut offset = -2;
+    let mut last_line = -1;
     let cause_formatter = trimmed_causes.iter().fold(String::new(), |acc, (cause, line, pos)| {
         let source_line = if *line > 0 { source.lines().nth(*line as usize - 1) } else { Some("") };
 
-        offset += 2;
-        acc + &format!(
-            "     |{} |- {}\n     |{}^ {} ({}:{})\n",
-            String::from_utf8(vec![b' '; 2 + (err.pos + offset) as usize]).unwrap(),
-            source_line.unwrap_or(""),
-            String::from_utf8(vec![b' '; (err.pos + pos + 5 + offset) as usize]).unwrap(),
-            cause,
-            line,
-            pos,
-        )
+        if last_line != *line {
+            last_line = *line;
+            offset += 2;
+            acc + &format!(
+                "     |{} |- {}\n     |{}^ {} ({}:{})\n",
+                String::from_utf8(vec![b' '; 2 + (err.pos + offset) as usize]).unwrap(),
+                source_line.unwrap_or(""),
+                String::from_utf8(vec![b' '; (err.pos + pos + 5 + offset) as usize]).unwrap(),
+                cause,
+                line,
+                pos,
+            )
+        } else {
+            acc + &format!(
+                "     |{}^ {} ({}:{})\n",
+                String::from_utf8(vec![b' '; (err.pos + pos + 5 + offset) as usize]).unwrap(),
+                cause,
+                line,
+                pos,
+            )
+        }
     });
 
     format!(
