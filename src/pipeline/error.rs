398fdfebf04332ed93af644adf8f56a382990bdb
Joel Abrahams <abrahamsjo@gmail.com>
2019-08-01 16:12:17 +0200

    Reverse order in which lines are displayed
    Now the syntax error messages are slightly easier to read

--- src/pipeline/error.rs
+++ src/pipeline/error.rs
@@ -5,35 +5,35 @@ use std::path::PathBuf;
 const SYNTAX_ERR_MAX_DEPTH: usize = 2;
 
 pub fn syntax_err(err: &ParseErr, source: &str, in_path: &PathBuf) -> String {
-    let cause_formatter = &err.causes[0..min(err.causes.len(), SYNTAX_ERR_MAX_DEPTH)].iter().fold(
-        String::new(),
-        |acc, cause| {
+    let cause_formatter = &err.causes[0..min(err.causes.len(), SYNTAX_ERR_MAX_DEPTH)]
+        .iter()
+        .rev()
+        .fold(String::new(), |acc, cause| {
             acc + &format!(
-                "     | {:3} |- {}\n     | {}^ in {} ({}:{})\n",
+                "{:3}  |- {}\n     | {}^ in {} ({}:{})\n",
                 cause.line,
                 source.lines().nth(cause.line as usize - 1).unwrap_or(""),
-                String::from_utf8(vec![b' '; cause.pos as usize + 6]).unwrap(),
+                String::from_utf8(vec![b' '; cause.pos as usize]).unwrap(),
                 cause.cause,
                 cause.line,
                 cause.pos,
             )
-        }
-    );
+        });
 
     format!(
         "--> {}:{}:{}
      | {}
-     | {:3} |- {}
-     | {}{}
-{}",
+{}
+{:3}  |- {}
+     | {}{}",
         in_path.display(),
         err.line,
         err.pos,
         err.msg,
+        cause_formatter,
         err.line,
         source.lines().nth(err.line as usize - 1).unwrap_or(""),
-        String::from_utf8(vec![b' '; err.pos as usize + 6]).unwrap(),
-        String::from_utf8(vec![b'^'; err.width as usize]).unwrap(),
-        cause_formatter
+        String::from_utf8(vec![b' '; err.pos as usize]).unwrap(),
+        String::from_utf8(vec![b'^'; err.width as usize]).unwrap()
     )
 }
