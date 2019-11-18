c861aa1d2744c06e1c79565a1ff7aedfd5911ffd
Joel Abrahams <abrahamsjo@gmail.com>
2019-11-18 18:04:38 +0100

    Update python command in linux

--- tests/output/common.rs
+++ tests/output/common.rs
@@ -1,5 +1,5 @@
 #[cfg(target_os = "linux")]
-pub static PYTHON: &str = "python3";
+pub static PYTHON: &str = "python3.8";
 #[cfg(target_os = "macos")]
 pub static PYTHON: &str = "python3";
 #[cfg(target_os = "windows")]
