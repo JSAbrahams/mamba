f4b934fab3b5e401976eb04057db8e21d7986128
Joel Abrahams <abrahamsjo@gmail.com>
2020-01-04 12:02:57 +0100

    Create constraint struct

--- src/type_checker/mod.rs
+++ src/type_checker/mod.rs
@@ -10,6 +10,7 @@ use crate::type_checker::type_result::TypeResults;
 pub mod context;
 pub mod environment;
 
+mod constraints;
 mod infer;
 mod modify;
 mod util;
@@ -17,24 +18,15 @@ mod util;
 pub mod type_result;
 pub type CheckInput = (AST, Option<String>, Option<PathBuf>);
 
-// TODO make type checker modify AST where necessary for more advanced language
-// features
-
 /// Checks whether a given [AST](crate::parser::ast::AST) is well
 /// typed according to the specification of the language.
 ///
 /// Should never panic.
 ///
-/// # Examples
-///
-/// // examples here
-///
 /// # Failures
 ///
 /// Any ill-typed [AST](crate::parser::ast::AST) results in a
 /// failure.
-///
-/// // failure examples here
 pub fn check_all(inputs: &[CheckInput]) -> TypeResults {
     let context = Context::try_from(inputs)?.into_with_primitives()?.into_with_std_lib()?;
     let inputs: Vec<CheckInput> = inputs
