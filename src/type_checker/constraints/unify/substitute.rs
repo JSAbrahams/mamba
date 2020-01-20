d80b863366aeb3ebf828a63b7be5a46179a66344
Joel Abrahams <abrahamsjo@gmail.com>
2020-01-20 22:33:16 +0100

    Use recursion in unify
    This prevents us from accidentally creating new lists
    and ignoring the old ones.
    Note that in some situations we grow the constraint
    list, but whenever we do this we take care to append
    it to existing constraints.
    
    Use a while loop in the substitution phase, since that
    does make more sense there then to do it recursively.

--- src/type_checker/constraints/unify/substitute.rs
+++ src/type_checker/constraints/unify/substitute.rs
@@ -2,10 +2,17 @@ use crate::type_checker::constraints::constraint::expected::Expected;
 use crate::type_checker::constraints::constraint::Constraints;
 use crate::type_checker::type_result::TypeResult;
 
-pub fn substitute(old: &Expected, new: &Expected, constr: &Constraints) -> TypeResult<Constraints> {
+pub fn substitute(
+    old: &Expected,
+    new: &Expected,
+    constr: &Constraints,
+    is_sub: bool
+) -> TypeResult<Constraints> {
     println!(
-        "{:width$} [subs] {} <= {}",
+        "{:width$} [subs {}{}] {} <= {}",
         format!("({}<={})", old.pos, new.pos),
+        constr.constraints.len(),
+        if is_sub { " in sub" } else { "" },
         old.expect,
         new.expect,
         width = 30
@@ -14,7 +21,9 @@ pub fn substitute(old: &Expected, new: &Expected, constr: &Constraints) -> TypeR
 }
 
 fn sub_inner(old: &Expected, new: &Expected, constr: &mut Constraints) -> TypeResult<Constraints> {
-    if let Some(constraint) = constr.constraints.pop() {
+    let mut substituted = Constraints::new();
+
+    while let Some(constraint) = constr.constraints.pop() {
         let (left, right) = (constraint.0, constraint.1);
         macro_rules! replace {
             () => {{
@@ -29,6 +38,7 @@ fn sub_inner(old: &Expected, new: &Expected, constr: &mut Constraints) -> TypeRe
         } else {
             left
         };
+
         let right = if &right == old {
             replace!();
             Expected::new(&right.pos, &new.expect)
@@ -36,9 +46,8 @@ fn sub_inner(old: &Expected, new: &Expected, constr: &mut Constraints) -> TypeRe
             right
         };
 
-        let mut unified = Constraints::new().add(&left, &right);
-        Ok(unified.append(&sub_inner(old, new, constr)?))
-    } else {
-        Ok(constr.clone())
+        substituted.push(&left, &right);
     }
+
+    Ok(substituted)
 }
--- src/type_checker/constraints/unify/unify_link.rs
+++ src/type_checker/constraints/unify/unify_link.rs
@@ -6,7 +6,7 @@ use itertools::{EitherOrBoth, Itertools};
 use crate::common::position::Position;
 use crate::type_checker::constraints::constraint::expected::Expect::*;
 use crate::type_checker::constraints::constraint::expected::Expected;
-use crate::type_checker::constraints::constraint::{Constraint, Constraints};
+use crate::type_checker::constraints::constraint::Constraints;
 use crate::type_checker::constraints::unify::substitute::substitute;
 use crate::type_checker::constraints::Unified;
 use crate::type_checker::context::function;
@@ -21,34 +21,35 @@ use crate::type_checker::type_result::TypeErr;
 /// Otherwise, we have to make a entirely new copy of the list of all
 /// constraints each time we do a recursive call to unify link.
 pub fn unify_link(constr: &mut Constraints, sub: &Constraints, ctx: &Context) -> Unified {
-    while let Some(constraint) = constr.constraints.pop() {
-        let constraint = match (&constraint.0.expect, &constraint.1.expect) {
-            (ExpressionAny, ExpressionAny) | (Truthy, Truthy) | (RaisesAny, RaisesAny) =>
-                Ok(Constraints::from(&constraint)),
-
-            (Expression { .. }, ExpressionAny) | (ExpressionAny, Expression { .. }) =>
-                Ok(Constraints::from(&constraint)),
-            (Truthy, ExpressionAny) | (ExpressionAny, Truthy) => Ok(Constraints::from(&constraint)),
-            (Type { .. }, ExpressionAny) | (ExpressionAny, Type { .. }) =>
-                Ok(Constraints::from(&constraint)),
+    if let Some(constraint) = constr.constraints.pop() {
+        match (&constraint.0.expect, &constraint.1.expect) {
+            (ExpressionAny, ExpressionAny)
+            | (Truthy, Truthy)
+            | (RaisesAny, RaisesAny)
+            | (Expression { .. }, ExpressionAny)
+            | (ExpressionAny, Expression { .. })
+            | (Truthy, ExpressionAny)
+            | (ExpressionAny, Truthy)
+            | (Type { .. }, ExpressionAny)
+            | (ExpressionAny, Type { .. }) => unify_link(constr, sub, ctx),
 
             (Expression { .. }, Expression { .. })
             | (Type { .. }, Expression { .. })
             | (Expression { .. }, Type { .. }) => {
-                let mut constr = substitute(&constraint.0, &constraint.1, &constr)?;
+                let mut constr = substitute(&constraint.0, &constraint.1, &constr, false)?;
                 let mut subst = Constraints::from(&constraint);
-                subst.append(&substitute(&constraint.0, &constraint.1, &sub)?);
+                subst.append(&substitute(&constraint.0, &constraint.1, &sub, true)?);
                 unify_link(&mut constr, &subst, ctx)
             }
 
             (Type { type_name: left }, Type { type_name: right }) if left == right => {
                 ctx.lookup(left, &constraint.0.pos)?;
-                Ok(Constraints::from(&constraint))
+                unify_link(constr, sub, ctx)
             }
             (Type { type_name }, Truthy) | (Truthy, Type { type_name }) => {
                 let expr_ty = ctx.lookup(type_name, &constraint.0.pos)?;
                 expr_ty.fun_args(&TypeName::from(function::python::TRUTHY), &constraint.0.pos)?;
-                Ok(Constraints::from(&constraint))
+                unify_link(constr, sub, ctx)
             }
             (Type { type_name: left }, Type { type_name: right }) => Err(vec![TypeErr::new(
                 &Position::default(),
@@ -64,78 +65,67 @@ pub fn unify_link(constr: &mut Constraints, sub: &Constraints, ctx: &Context) ->
                 } else {
                     f_name.clone()
                 };
-                let possible: HashSet<Vec<FunctionArg>> =
-                    expr_ty.fun_args(&f_name, &constraint.0.pos)?;
-
-                for f_args in possible {
-                    for either_or_both in f_args.iter().zip_longest(args.iter()) {
-                        match either_or_both {
-                            EitherOrBoth::Both(fun_arg, expected) => {
-                                let ty = &fun_arg.ty.as_ref();
-                                let type_name = ty
-                                    .ok_or_else(|| {
-                                        TypeErr::new(
-                                            &expected.pos,
-                                            "Function argument must have type parameters"
-                                        )
-                                    })?
-                                    .clone();
-                                constr.push(
-                                    &expected,
-                                    &Expected::new(&expected.pos, &Type { type_name })
-                                )
-                            }
-                            EitherOrBoth::Left(fun_arg) if !fun_arg.has_default =>
-                                return Err(vec![TypeErr::new(
-                                    &constraint.0.pos,
-                                    "Expected argument"
-                                )]),
-                            EitherOrBoth::Right(expected) =>
-                                return Err(vec![TypeErr::new(
-                                    &expected.pos,
-                                    "Unexpected argument"
-                                )]),
-                            _ => {}
-                        }
-                    }
-                }
-
-                unify_link(constr, &sub, ctx)
+                let possible = expr_ty.fun_args(&f_name, &constraint.0.pos)?;
+                let mut constr = unify_fun_arg(possible, args, constr, &constraint.0.pos)?;
+                unify_link(&mut constr, &sub, ctx)
             }
 
             (Truthy, Expression { .. }) | (Expression { .. }, Truthy) => unify_link(
-                &mut substitute(&constraint.0, &constraint.1, constr)?,
-                &substitute(&constraint.0, &constraint.1, &sub)?.add_constraint(&constraint),
+                &mut substitute(&constraint.0, &constraint.1, constr, false)?,
+                &substitute(&constraint.0, &constraint.1, &sub, true)?.add_constraint(&constraint),
                 ctx
             ),
 
             (Mutable { expect }, _) => {
-                let constraint =
-                    Constraint(Expected::new(&constraint.0.pos, expect.deref()), constraint.1);
-                Ok(Constraints::from(&constraint))
+                constr.push(&Expected::new(&constraint.0.pos, expect.deref()), &constraint.0);
+                unify_link(constr, sub, ctx)
             }
             (_, Mutable { expect }) => {
-                let constraint =
-                    Constraint(constraint.0, Expected::new(&constraint.1.pos, expect.deref()));
-                Ok(Constraints::from(&constraint))
+                constr.push(&constraint.0, &Expected::new(&constraint.1.pos, expect.deref()));
+                unify_link(constr, sub, ctx)
             }
 
             _ => panic!(
                 "Unexpected: {}={} : {} == {}",
                 constraint.0.pos, constraint.1.pos, constraint.0.expect, constraint.1.expect
             )
-        }?;
+        }
+    } else {
+        Ok(constr.clone())
+    }
+}
 
-        for constraint in &constraint.constraints {
-            println!(
-                "{:width$} [unif] {} == {}",
-                format!("({},{})", constraint.0.pos, constraint.1.pos),
-                constraint.0.expect,
-                constraint.1.expect,
-                width = 30
-            );
+fn unify_fun_arg(
+    possible: HashSet<Vec<FunctionArg>>,
+    args: &Vec<Expected>,
+    constr: &Constraints,
+    pos: &Position
+) -> Unified {
+    let mut constr = constr.clone();
+
+    for f_args in possible {
+        for either_or_both in f_args.iter().zip_longest(args.iter()) {
+            match either_or_both {
+                EitherOrBoth::Both(fun_arg, expected) => {
+                    let ty = &fun_arg.ty.as_ref();
+                    let type_name = ty
+                        .ok_or_else(|| {
+                            TypeErr::new(
+                                &expected.pos,
+                                "Function argument must have type parameters"
+                            )
+                        })?
+                        .clone();
+                    constr.push(&expected, &Expected::new(&expected.pos, &Type { type_name }))
+                }
+                EitherOrBoth::Left(fun_arg) if !fun_arg.has_default =>
+                    return Err(vec![TypeErr::new(&pos, "Expected argument")]),
+                EitherOrBoth::Right(expected) =>
+                    return Err(vec![TypeErr::new(&expected.pos, "Unexpected argument")]),
+                _ => {}
+            }
         }
     }
 
-    Ok(constr.clone())
+    Ok(constr)
 }
