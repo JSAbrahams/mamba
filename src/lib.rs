de2cbb6e94734489f6fee61475b45d2cab550cb7
Joel Abrahams <abrahamsjo@gmail.com>
2022-05-19 16:01:14 +0200

    Add some smoke tests for executable
    - Test that a target dir is created.
    - Test that one can use a custom source.
    - Test that one can use a custom target.
    - Test that an error is given if the source file,
      or directory, does not exist.
    - Test compiling of source file instead of dir.
    - Test that error message contains relative path
      not an absolute one.
    - Remove link to doc in command line help

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate ansi_term;
 extern crate log;
 extern crate loggerv;
 
-use std::ffi::OsStr;
 use std::fs::create_dir;
 use std::path::{Path, PathBuf};
 
@@ -49,6 +48,14 @@ pub fn transpile_directory(
     target: Option<&str>,
 ) -> Result<PathBuf, Vec<(String, String)>> {
     let src_path = source.map_or(current_dir.join(SOURCE), |p| current_dir.join(p));
+    if !src_path.is_file() && !src_path.is_dir() {
+        let msg = format!("Source directory does not exist: {}", src_path.as_os_str().to_str().unwrap());
+        return Err(vec![(String::from("pathfinding"), msg)]);
+    } else if src_path.is_file() && !src_path.exists() {
+        let msg = format!("Source file does not exist: {}", src_path.as_os_str().to_str().unwrap());
+        return Err(vec![(String::from("pathfinding"), msg)]);
+    }
+
     let out_dir = current_dir.join(target.unwrap_or(TARGET));
     if !out_dir.exists() {
         create_dir(&out_dir).map_err(|e| vec![(String::from("io"), e.to_string())])?;
@@ -80,7 +87,7 @@ pub fn transpile_directory(
     let source_pairs = sources.iter().zip(in_absolute_paths.iter());
     let source_option_pairs: Vec<_> =
         source_pairs.map(|(source, path)| (source.clone(), Some(path.clone()))).collect();
-    let mamba_source = mamba_to_python(source_option_pairs.as_slice(), current_dir)?;
+    let mamba_source = mamba_to_python(source_option_pairs.as_slice(), &src_path)?;
 
     for (source, out_path) in mamba_source.iter().zip(out_absolute_paths) {
         let out_path = out_path.with_extension("py");
@@ -96,19 +103,18 @@ pub fn transpile_directory(
 /// messages. This path is not necessary however.
 pub fn mamba_to_python(
     source: &[(String, Option<PathBuf>)],
-    source_dir: &Path,
+    source_dir: &PathBuf,
 ) -> Result<Vec<String>, Vec<(String, String)>> {
     // Strip until source
-    let strip_prefix = |p: PathBuf| p.strip_prefix(source_dir)
-        .map(|p| {
-            let dir: &OsStr = source_dir.iter().last().unwrap_or("".as_ref());
-            PathBuf::from(PathBuf::from(&dir).join(p))
-        })
-        .unwrap_or(p);
-    let source: Vec<ParseInput> = source
-        .iter()
-        .map(|(src, path)| (src.clone(), path.clone().map(strip_prefix)))
-        .collect();
+    let strip_prefix = |p: PathBuf| {
+        p.strip_prefix(source_dir)
+            .map(|p| {
+                PathBuf::from(&source_dir.iter().last().unwrap_or_else(|| "".as_ref())).join(p)
+            })
+            .unwrap_or(p)
+    };
+    let source: Vec<ParseInput> =
+        source.iter().map(|(src, path)| (src.clone(), path.clone().map(strip_prefix))).collect();
 
     let asts = parse_all(&source).map_err(|errs| {
         errs.iter()
