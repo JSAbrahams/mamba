d339eaccb9a4ef6b7c17bd191a328346db6875eb
Joël Abrahams <JSAbrahams@users.noreply.github.com>
2022-05-19 10:55:08 +0200

    Use write! not format! in delimit functions (#286)
    * Use writer in delimit functions
    
    * Remove magic number in Core
    
    * Remove pipeline dir
    
    This didn't really do much, all the methods can
    easily just be in the main.
    
    io is small enough and important enough to just be
    to-level in my eyes.
    
    - Let test code replace CRLF with LF line endings,
      so we don't constantly have to deal with this.
    
    * Use writeln for lines which end in single newline

--- src/common/delimit.rs
+++ src/common/delimit.rs
@@ -1,28 +1,29 @@
 use std::fmt::Display;
+use std::fmt::Write;
 
 pub fn custom_delimited<I, D>(iterable: I, delimiter: &str, prepend: &str) -> String
     where
-        I: IntoIterator<Item=D>,
+        I: IntoIterator<Item=D> + Clone,
         D: Display {
-    let mut string = String::new();
-    iterable.into_iter().for_each(|item| string.push_str(&format!("{}{}", item, delimiter)));
-    if !string.is_empty() {
-        format!("{}{}", prepend, string.trim_end_matches(delimiter))
-    } else {
-        String::new()
+    if iterable.clone().into_iter().next().is_none() {
+        return String::new();
     }
+
+    let mut s = String::from(prepend);
+    iterable.into_iter().for_each(|item| write!(&mut s, "{}{}", item, delimiter).unwrap());
+    String::from(s.trim_end_matches(delimiter))
 }
 
 pub fn comma_delm<I, D>(iterable: I) -> String
     where
-        I: IntoIterator<Item=D>,
+        I: IntoIterator<Item=D> + Clone,
         D: Display {
     custom_delimited(iterable, ", ", "")
 }
 
 pub fn newline_delimited<I, D>(iterable: I) -> String
     where
-        I: IntoIterator<Item=D>,
+        I: IntoIterator<Item=D> + Clone,
         D: Display {
     custom_delimited(iterable, "\n", "")
 }
