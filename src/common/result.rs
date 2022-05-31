6e15c1391b8c87ea50e66f7a4272e2a1ffe3aada
Joel Abrahams <abrahamsjo@gmail.com>
2022-05-31 20:57:32 +0200

    Generalize and_or_a
    - Make how types are printed more consistent

--- src/common/result.rs
+++ src/common/result.rs
@@ -1,5 +1,21 @@
+use std::fmt::Display;
 use std::path::PathBuf;
 
 pub trait WithSource {
     fn with_source(self, source: &Option<String>, path: &Option<PathBuf>) -> Self;
+}
+
+pub fn an_or_a<D>(parsing: D) -> &'static str where D: Display {
+    let parsing = format!("{}", parsing).to_ascii_lowercase();
+
+    if let Some('s') = parsing.chars().last() {
+        return "";
+    } else if parsing.chars().next().is_none() {
+        return "";
+    }
+
+    match parsing.chars().next() {
+        Some(c) if ['a', 'e', 'i', 'o', 'u'].contains(&c.to_ascii_lowercase()) => "an ",
+        _ => "a "
+    }
 }
