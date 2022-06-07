82930a561bc9e3f6e3c80d2d78faa907259223bd
Joël Abrahams <JSAbrahams@users.noreply.github.com>
2022-06-07 23:11:41 +0200

    Fix check access of nested fields of object (#339)
    * Add test for accessing nested fields
    
    * Remove unnecessary constraint generation
    
    * Rethink constraint gen for property call
    
    Now, we generate a chain of calls and make this
    equivalent to the entire AST.
    We also do this for the nested calls:
    
    'a.b.c' is turned into:
    `a.b.c` == `a.b`.c
    `a.b` == `a`.b
    
    In theory, the unification stage should then be
    able to pick off from here, but it doesn't yet.
    
    * Improve spacing in trace messages
    
    * Simplify how access is generated
    
    Instead of creating a long chain, we generate a
    single access constraint for each level of the
    access.
    
    `x.y.z`
    becomes:
    `x`.y == `x.y`
    `xy`.z == `x.y.z`
    instead of:
    `x`.y == `x.y`
    `x`.`y`.z == `x.y.z`
    which the unification stage had trouble with.
    
    * Add extra check in call
    
    Should prevent panics in the event that we somehow
    end up there.
    
    * Use match instead of if chain

--- tests/resource/valid/class/print_types_double_nested_check.py
+++ tests/resource/valid/class/print_types_double_nested_check.py
@@ -8,13 +8,4 @@ class X:
         self.y = Y(a)
 
 x: X = X(10)
-
-x.y.a = x.y.a + 2
-x.y.a = x.y.a - 3
-x.y.a = x.y.a * 6
-
-x.y.a = x.y.a / 7
-x.y.a = x.y.a ** 2
-
-x.y.a = x.y.a << 10
-x.y.a = x.y.a >> 5
+print(x.y.a)
