9421a66972d13367d447c9560bf2f133c9ce990c
Joel Abrahams <abrahamsjo@gmail.com>
2022-06-10 12:21:43 +0200

    Test tuple has collection parent

--- src/check/context/clss/concrete/mod.rs
+++ src/check/context/clss/concrete/mod.rs
@@ -1,2 +1,24 @@
 pub mod union;
 pub mod variant;
+
+#[cfg(test)]
+mod test {
+    use crate::check::context::{clss, LookupClass};
+    use crate::check::context::clss::HasParent;
+    use crate::check::name::Name;
+    use crate::check::name::true_name::TrueName;
+    use crate::check::result::TypeResult;
+    use crate::common::position::Position;
+    use crate::Context;
+
+    #[test]
+    fn tuple_has_collection_parent() -> TypeResult<()> {
+        let ctx = Context::default().into_with_std_lib()?.into_with_primitives()?;
+
+        let pos = Position::default();
+        let tuple = ctx.class(&Name::from(clss::TUPLE), pos)?;
+
+        assert!(tuple.has_parent(&TrueName::from(clss::TUPLE), &ctx, pos)?);
+        Ok(())
+    }
+}
