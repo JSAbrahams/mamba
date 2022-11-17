e90633deb81b57fd5d96d3f5e7fd91deebae0806
Joël Abrahams <JSAbrahams@users.noreply.github.com>
2022-11-17 18:23:06 +0100

    Fix Windows build (#361)
    * Improve error message of testing python output
    
    * Print resource path not resource contents
    
    * Change python command for windows cfg
    
    Likely not the source of the error, as it states
    that it cannot find the file specified (most
    likely the resource).
    Still worth a try.

--- tests/system/common.rs
+++ tests/system/common.rs
@@ -3,4 +3,4 @@ pub static PYTHON: &str = "python3.10";
 #[cfg(target_os = "macos")]
 pub static PYTHON: &str = "python3";
 #[cfg(target_os = "windows")]
-pub static PYTHON: &str = "py";
+pub static PYTHON: &str = "python";
