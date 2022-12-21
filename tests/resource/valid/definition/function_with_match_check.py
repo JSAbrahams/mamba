a8fd1a1f893eb9840ee97323a3219c3b9ccf9183
Joel Abrahams <abrahamsjo@gmail.com>
2022-12-21 17:53:39 +0100

    Add returns and assigns recursively
    We go into the branches of relevant core modules.
    Only try except cannot be tested yet as we don't
    yet have error handling properly implemented.
    
    - Also remove some redundant application logic

---
+++ tests/resource/valid/definition/function_with_match_check.py
@@ -0,0 +1,8 @@
+def f(x: int) -> int:
+    match x:
+        case 2:
+            return 3
+        case 4:
+            return 30
+        case _:
+            return 300
---
+++ tests/resource/valid/definition/function_with_nested_if_check.py
@@ -0,0 +1,8 @@
+def f(x: int) -> int:
+    if x > 10:
+        if x > 30:
+            return x + 1
+        else:
+            return x - 1
+    else:
+        return x
