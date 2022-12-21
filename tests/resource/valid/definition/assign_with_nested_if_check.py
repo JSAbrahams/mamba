9b9c22e4b0cd284f6617c731c0af3e4a2cbd5b02
Joel Abrahams <abrahamsjo@gmail.com>
2022-12-21 19:26:34 +0100

    Add test for augmenting ret and assign in block

--- tests/resource/valid/definition/assign_with_nested_if_check.py
+++ tests/resource/valid/definition/assign_with_nested_if_check.py
@@ -1,6 +1,7 @@
 x: int = 20
 if x > 10:
     if x > 30:
+        print("string")
         a = x + 1
     else:
         a = x - 1
--- tests/resource/valid/definition/function_with_nested_if_check.py
+++ tests/resource/valid/definition/function_with_nested_if_check.py
@@ -1,6 +1,7 @@
 def f(x: int) -> int:
     if x > 10:
         if x > 30:
+            print("string")
             return x + 1
         else:
             return x - 1
