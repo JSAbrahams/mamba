0af19f0ac84fc70ac08738c21b825cf9e6df7653
Joel Abrahams <abrahamsjo@gmail.com>
2022-12-31 18:47:01 +0100

    Verify condition must be booleans
    And that all variables must be defined.

--- tests/check/invalid/collection.rs
+++ tests/check/invalid/collection.rs
@@ -4,8 +4,20 @@ use mamba::parse::parse;
 use crate::common::resource_content;
 
 #[test]
+fn builder_with_cond_not_boolean() {
+    let source = resource_content(false, &["type", "collection"], "builder_with_cond_not_boolean.mamba");
+    check_all(&[*parse(&source).unwrap()]).unwrap_err();
+}
+
+#[test]
 fn conflicting_collection_types() {
     let source = resource_content(false, &["type", "collection"], "conflicting_collection_types.mamba");
+    check_all(&[*parse(&source).unwrap()]).unwrap_err();
+}
+
+#[test]
+fn builder_with_undefined_var() {
+    let source = resource_content(false, &["type", "collection"], "builder_with_undefined_var.mamba");
     check_all(&[*parse(&source).unwrap()]).unwrap_err();
 }
 
