dce72106ab1671caa9ba98937c166673cbccc582
Joel Abrahams <joel@q-bird.nl>
2025-06-25 08:49:10 +0200

    chore: fix flake, outsource rust tooling to rustup

--- flake.nix
+++ flake.nix
@@ -1,3 +1,7 @@
+# Slighty modified example of example flake at <https://nixos.wiki/wiki/Rust> to install Rust.
+# We opt for the variant where we outsource Rust toolchain management to rustup.
+# This is so that those who wish to develop without Nix (for any number of reasons) still benefit from Rustup.
+
 {
 description = "A Nix flake providing a Rust dev environment with rustup, cargo tooling, and nushell";
 
@@ -9,17 +13,20 @@ inputs = {
 outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-        pkgs         = import nixpkgs { inherit system; };
-        rustPlatform = pkgs.rustPlatform;
+        pkgs = import nixpkgs { inherit system; };
+        # Read the rust toolchain config relative to the flake's root
+        overrides = (builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml")));
+        libPath = with pkgs; lib.makeLibraryPath [
+          # load external libraries that you need in your rust project here
+        ];
     in {
         devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
                 git               # Version control tool
                 
-                rustc             # Rust compiler
-                cargo             # Rust package manager
-                cargo-nextest     # Fast, parallel test runner
-                cargo-sort        # Sorts Cargo.toml entries
+                clang                 # C++ tooling
+                llvmPackages.bintools #
+                rustup                # Manage rust toolchains
                 
                 nushell           # Nu Shell                   <https://wiki.nixos.org/wiki/Nushell>
                 starship          # Display relevant info      <https://wiki.nixos.org/wiki/Starship>
@@ -27,7 +34,31 @@ outputs = { self, nixpkgs, flake-utils, ... }:
                 openssh           # SSH agent
             ];
 
+            RUSTC_VERSION = overrides.toolchain.channel;
+            # https://github.com/rust-lang/rust-bindgen#environment-variables
+            LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+            # Add precompiled library to rustc search path
+            RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+                # add libraries here (e.g. pkgs.libvmi)
+            ]);
+            LD_LIBRARY_PATH = libPath;
+            # Add glibc, clang, glib, and other headers to bindgen search path
+            # Includes normal include path
+            BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
+                # add dev libraries here (e.g. pkgs.libvmi.dev)
+                pkgs.glibc.dev
+            ])
+            # Includes with special directory paths
+            ++ [
+                ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+                ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+                ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+            ];
+
             shellHook = ''
+                export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+                export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+
                 # make sure to configure githooks path
                 git config core.hooksPath .githooks
                 
