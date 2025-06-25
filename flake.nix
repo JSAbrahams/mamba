ecffbb40660d0a2fa0e3aba8ceea33f00ce28a28
Joel Abrahams <joel@q-bird.nl>
2025-06-25 10:37:45 +0200

    fix: make sure that linker is configured in flake

--- flake.nix
+++ flake.nix
@@ -44,16 +44,15 @@ outputs = { self, nixpkgs, flake-utils, ... }:
             LD_LIBRARY_PATH = libPath;
             # Add glibc, clang, glib, and other headers to bindgen search path
             # Includes normal include path
-            BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
+            BINDGEN_EXTRA_CLANG_ARGS =
                 # add dev libraries here (e.g. pkgs.libvmi.dev)
-                pkgs.glibc.dev
-            ])
-            # Includes with special directory paths
-            ++ [
-                ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-                ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-                ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-            ];
+                (builtins.map (a: "-I${a}/include") [ pkgs.glibc.dev ])
+                # Includes with special directory paths
+                ++ [
+                    "-I${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"
+                    "-I${pkgs.glib.dev}/include/glib-2.0"
+                    "-I${pkgs.glib.out}/lib/glib-2.0/include/"
+                ];
 
             shellHook = ''
                 export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
