d524868a3886bee52740f5b13388cda6b16a3c4d
Joel Abrahams <joel@q-bird.nl>
2026-08-25 11:44:07 +0200

    fix: reflect explicit step size

--- tests/resource/valid/access/index_via_function.py
+++ tests/resource/valid/access/index_via_function.py
@@ -1,12 +1,12 @@
 def f() -> slice:
-    return slice(0, 2 - 1)
+    return slice(0, 2 - 1, 1)
 def g() -> slice:
-    return slice(0, 2)
+    return slice(0, 2, 1)
 
 def i() -> range:
-    return range(0, 2)
+    return range(0, 2, 1)
 def j() -> range:
-    return range(0, 2 + 1)
+    return range(0, 2 + 1, 1)
 
 x: list[int] = [1, 2, 3]
 x[f()]
